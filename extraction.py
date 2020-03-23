fc96632b135cc5611e614fc94931b785be2df46e
ken8594 <62523933+ken8594@users.noreply.github.com>
2020-03-23 11:24:48 -0500

    Refactored some code to be loopable

--- extraction.py
+++ extraction.py
@@ -1,39 +1,33 @@
+import re
+
 import pandas as pd
 import nltk
 from difflib import SequenceMatcher
 from rake_nltk import Rake
+from nltk.corpus import stopwords
 
-experience = ""
-r = Rake()
-r.extract_keywords_from_text(experience)
-phraselist = r.get_ranked_phrases()
-skillslist = []
-f = open("sample-data/skills.txt", "r")
-for x in f:
-  skillslist.append(x.lower())
 
-def Remove(duplicate):
+def list_dedupe(list_data):
     final_list = []
-    for num in duplicate:
-        if num not in final_list:
-            final_list.append(num)
+    for list_entry in list_data:
+        if list_entry not in final_list:
+            final_list.append(list_entry)
     return final_list
 
-experienceduration = []
-setofskills = []
-setofphrases = []
 
-def experiencelevel():
+def experience_level():
     for phrase in phraselist:
-        listoftimes = ["century", "centuries", "decades", "decade", "years", "year", "months", "month", "weeks", "week", "days", "day", "hours", "hour", "minutes", "minute", "seconds", "second"]
+        listoftimes = ["century", "centuries", "decades", "decade", "years", "year", "months", "month", "weeks", "week",
+                       "days", "day", "hours", "hour", "minutes", "minute", "seconds", "second"]
         for time in listoftimes:
             if any(str.isdigit(c) for c in phrase) and time in phrase:
                 experienceduration.append(phrase)
                 phraselist.remove(phrase)
                 break
-    return Remove(experienceduration)
+    return list_dedupe(experienceduration)
 
-def getskills():
+
+def get_skills():
     for phrase in phraselist:
         compsim = 0
         compskill = ""
@@ -45,7 +39,6 @@ def getskills():
                         compsim = similarity
                         if compskill in setofskills:
                             setofskills.remove(compskill)
-                        compskill = skill.replace("\n","")
                         setofphrases.append(phrase)
                         setofskills.append(compskill)
             else:
@@ -54,7 +47,44 @@ def getskills():
                         compsim = similarity
                         if compskill in setofskills:
                             setofskills.remove(compskill)
-                        compskill = skill.replace("\n","")
                         setofphrases.append(phrase)
                         setofskills.append(compskill)
-    return Remove(setofphrases)
+    return list_dedupe(setofphrases)
+
+
+stop_words = set(stopwords.words("english"))
+# Creating a list of custom stopwords
+new_words = ["using", "show", "result", "large", "also", "iv", "one", "two", "new", "previously", "shown", "well",
+             "recently", "includes", "may", "im", "etc", "grad", "actually", "working", "worked", "ideal", "experience",
+             "able", "additionally", "use", "got", "work", "states", "lots", "say", "much", "appropriate", "ask", "starting"
+             "considering", "thing", "willing", "hoping", "helping", "went", "sent", "glad", "witch", "used", "done",
+             "sure", "items", "description", "helping", "experienced", "ask"]
+stop_words = stop_words.union(new_words)
+
+skillslist = []
+
+dataset = pd.read_csv('sample-data/experience.csv')
+experienceData = dataset.values.tolist()
+
+f = open("sample-data/skills.txt", "r")
+for x in f:
+    skillslist.append(str(x).lower().strip())
+
+
+for experience in experienceData:
+    experienceText = experience[2]
+    if type(experienceText) == float:
+        continue
+    r = Rake(stopwords=stop_words)
+    # Strip Links
+    experienceText = re.sub(r'^https?://.*[\r\n]*', '', experienceText, flags=re.MULTILINE)
+
+    r.extract_keywords_from_text(experienceText)
+    phraselist = r.get_ranked_phrases()
+
+    experienceduration = []
+    setofskills = []
+    setofphrases = []
+
+    print(experienceText)
+    print(get_skills())
